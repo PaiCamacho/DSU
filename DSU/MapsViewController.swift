b92290eac5b250864560003857c4cf06ff0e5066
Paola <camachopaola311@gmail.com>
2020-02-07 23:45:36 -0500

    Added basic location feature, zooms into user's location

--- DSU/MapsViewController.swift
+++ DSU/MapsViewController.swift
@@ -7,11 +7,15 @@
 //
 
 import UIKit
-
+import MapKit
 class MapsViewController: UIViewController {
-
+    private let locationManager = CLLocationManager()
+    private var currentCoordinate: CLLocationCoordinate2D?
+    
+    @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureLocationServices()
 
         // Do any additional setup after loading the view.
     }
@@ -26,5 +30,48 @@ class MapsViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    private func configureLocationServices(){
+        locationManager.delegate = self // View Contoller
+        let status = CLLocationManager.authorizationStatus()
+        if status == .notDetermined{
+            locationManager.requestWhenInUseAuthorization()
+        }
+        else if status == .authorizedAlways || status == .authorizedWhenInUse{
+            beginLocationUpdates(locationManager: locationManager)
+        }
+        
+    }
+    private func beginLocationUpdates(locationManager: CLLocationManager){
+        mapView.showsUserLocation = true
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+        
+    }
+    private func zoomToLatestLocation(with coordinate: CLLocationCoordinate2D){
+        let zoomRegion = MKCoordinateRegion(center: coordinate, latitudinalMeters: 1000,longitudinalMeters: 1000)
+        mapView.setRegion(zoomRegion, animated: true)
+    }
 
+}
+extension MapsViewController: CLLocationManagerDelegate{
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        guard let latestLocation = locations.first else { return }
+        
+        if currentCoordinate == nil{
+            zoomToLatestLocation(with: latestLocation.coordinate)
+            
+        }
+        
+        currentCoordinate = latestLocation.coordinate
+        
+    }
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedAlways || status == .authorizedWhenInUse{
+            beginLocationUpdates(locationManager: manager)
+        }
+    }
+
+    
 }
